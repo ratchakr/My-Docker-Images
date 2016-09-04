c079006bcd1e46ea3c574fe596df13c26eedcc5c
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2016-09-04 00:40:10 -0500

    spin off a couchbase node with preconfigured data,indexing, fts services and a data bucket created.

---
+++ couchbase-cluster/configure.sh
@@ -0,0 +1,22 @@
+set -m
+
+
+
+/entrypoint.sh couchbase-server &
+
+sleep 15
+
+echo "Calling CB REST services to set up the Couchbase Server with My Settings"
+
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
+
+curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Cfts
+
+curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+
+# Setup Memory Optimized Indexes
+curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+
+curl -v -X POST http://127.0.0.1:8091/pools/default/buckets -u Administrator:password -d 'name=testbucket' -d 'ramQuotaMB=200' -d 'authType=none' -d 'proxyPort=11222' 
+
+fg 1
---
+++ couchbase-cluster/start.sh
@@ -0,0 +1,6 @@
+#mkdir -p ~/couchbase/node1 ~/couchbase/node2 ~/couchbase/node3
+
+# cleanup - TODO
+
+docker-compose up
+
