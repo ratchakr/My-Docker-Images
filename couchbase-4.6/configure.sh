22dcb456a4e411157d3507dfdcb8a20e9283bc7a
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2017-03-15 20:10:59 -0500

    adding couchbase 4.6 docker files

---
+++ couchbase-4.6/configure.sh
@@ -0,0 +1,40 @@
+set -m
+
+echo "listing directories on the container"
+
+ls
+
+echo "docker host ip = " $DOCKER_HOST_IP 
+
+/entrypoint.sh couchbase-server &
+
+echo "sleeping..."
+
+sleep 30
+
+echo "Start configuring env by calling Rest endpoints"
+
+
+#curl -v -X POST http://192.168.99.100:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256 -d fullTextMemoryQuota=256
+curl -v -X POST "http://"$DOCKER_HOST_IP":8091/pools/default" -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
+
+sleep 5
+
+curl -v "http://"$DOCKER_HOST_IP":8091/node/controller/setupServices" -d 'services=kv%2Cn1ql%2Cindex%2Cfts'
+
+sleep 5
+
+curl -i -u Administrator:password -X POST "http://"$DOCKER_HOST_IP":8091/settings/indexes" -d 'storageMode=memory_optimized'
+
+curl -v -X POST "http://"$DOCKER_HOST_IP":8091/settings/web" -d port=8091 -d username=Administrator -d password=password
+
+echo "bucket set up start"
+
+echo "bucket name = " $BUCKET_NAME
+
+
+curl -v -X POST http://127.0.0.1:8091/pools/default/buckets -u Administrator:password -d "name="$BUCKET_NAME"" -d 'ramQuotaMB=200' -d 'authType=none' -d 'proxyPort=11222'
+
+echo "bucket set up done"
+
+fg 1
---
+++ couchbase-4.6/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "setting env"
+DOCKER_HOST_IP=${DOCKER_HOST_IP}
+
+echo "done" $DOCKER_HOST_IP
