6e68cbd33edf9f10899bc517bff09e4bb7354993
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2017-03-23 09:42:32 -0500

    add v2 of compose

--- couchbase-4.6/configure.sh
+++ couchbase-4.6/configure.sh
@@ -14,14 +14,11 @@ sleep 20
 
 echo "Start configuring env by calling REST endpoints"
 
-
-#curl -v -X POST http://192.168.99.100:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256 -d fullTextMemoryQuota=256
 curl -v -X POST "http://"$DOCKER_HOST_IP":8091/pools/default" -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
 
 sleep 5
 
 curl -v "http://"$DOCKER_HOST_IP":8091/node/controller/setupServices" -d 'services=kv%2Cn1ql%2Cindex%2Cfts'
-
 sleep 5
 
 curl -i -u Administrator:password -X POST "http://"$DOCKER_HOST_IP":8091/settings/indexes" -d 'storageMode=memory_optimized'
@@ -31,7 +28,6 @@ curl -v -X POST "http://"$DOCKER_HOST_IP":8091/settings/web" -d port=8091 -d use
 echo "bucket set up start"
 
 echo "bucket name = " $BUCKET_NAME
-
 
 curl -v -X POST http://127.0.0.1:8091/pools/default/buckets -u Administrator:password -d "name="$BUCKET_NAME"" -d 'ramQuotaMB=200' -d 'authType=none' -d 'proxyPort=11222'
 
