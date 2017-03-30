606c56299dffdbb2bb74e55f8721bda0c7de9c55
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2017-03-30 14:49:50 -0500

    remove use of DOCKER_HOST_IP while configuring couchbase server 4.6

--- couchbase-4.6/configure.sh
+++ couchbase-4.6/configure.sh
@@ -14,16 +14,16 @@ sleep 20
 
 echo "Start configuring env by calling REST endpoints"
 
-curl -v -X POST "http://"$DOCKER_HOST_IP":8091/pools/default" -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
 
 sleep 5
 
-curl -v "http://"$DOCKER_HOST_IP":8091/node/controller/setupServices" -d 'services=kv%2Cn1ql%2Cindex%2Cfts'
+curl -v http://127.0.0.1:8091/node/controller/setupServices -d 'services=kv%2Cn1ql%2Cindex%2Cfts'
 sleep 5
 
-curl -i -u Administrator:password -X POST "http://"$DOCKER_HOST_IP":8091/settings/indexes" -d 'storageMode=memory_optimized'
+curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 
-curl -v -X POST "http://"$DOCKER_HOST_IP":8091/settings/web" -d port=8091 -d username=Administrator -d password=password
+curl -v -X POST http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 
 echo "bucket set up start"
 
